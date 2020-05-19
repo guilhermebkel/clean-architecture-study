10ec432966dad50321c717d5a49de9c6d6bf552d
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-05-19 07:10:52 -0300

    test(signup): ensure SignUpController returns status code 400 if no email is supplied

--- app/src/presentation/controllers/SignUp.spec.ts
+++ app/src/presentation/controllers/SignUp.spec.ts
@@ -17,4 +17,21 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
