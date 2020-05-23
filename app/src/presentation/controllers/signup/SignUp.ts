759e030b43210f11b3d27902e1a96f2db6ef5868
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-05-23 14:13:50 -0300

    refactor(signup): move success response to helper

--- app/src/presentation/controllers/signup/SignUp.ts
+++ app/src/presentation/controllers/signup/SignUp.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './SignUpProtocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/HttpHelper'
+import { badRequest, serverError, ok } from '../../helpers/HttpHelper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -39,10 +39,7 @@ export class SignUpController implements Controller {
         password
       })
 
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
