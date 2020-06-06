80e83ccc944dfa93eb8b102db9e39c0b684f85a5
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-06 16:56:08 -0300

    tets(login): ensure LoginController returns 400 if no password is provided

--- app/src/presentation/controllers/login/Login.spec.ts
+++ app/src/presentation/controllers/login/Login.spec.ts
@@ -17,4 +17,18 @@ describe('LoginController', () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = new LoginController()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
 })
