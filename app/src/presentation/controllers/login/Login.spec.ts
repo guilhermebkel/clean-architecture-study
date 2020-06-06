5fe78020c4bc0b28f5239f4fb754eefd19889423
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-06 17:12:41 -0300

    refactor(login): unify code

--- app/src/presentation/controllers/login/Login.spec.ts
+++ app/src/presentation/controllers/login/Login.spec.ts
@@ -2,7 +2,7 @@ import { LoginController } from './Login'
 
 import { badRequest } from '../../helpers/HttpHelper'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { EmailValidator } from '../signup/SignUpProtocols'
+import { EmailValidator, HttpRequest } from '../signup/SignUpProtocols'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -28,6 +28,13 @@ const makeSut = (): SutTypes => {
     emailValidatorStub
   }
 }
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@email.com',
+    password: 'any_password'
+  }
+})
 
 describe('LoginController', () => {
   test('Should return 400 if no email is provided', async () => {
@@ -63,12 +70,7 @@ describe('LoginController', () => {
 
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
 
-    const httpRequest = {
-      body: {
-        email: 'any_email@email.com',
-        password: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
 
     const httpResponse = await sut.handle(httpRequest)
 
@@ -80,12 +82,7 @@ describe('LoginController', () => {
 
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
 
-    const httpRequest = {
-      body: {
-        email: 'any_email@email.com',
-        password: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
 
     await sut.handle(httpRequest)
 
