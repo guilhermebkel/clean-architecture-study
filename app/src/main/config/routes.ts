cf70c2186744e9ce5574e9e5437a34dbbd959607
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-11 10:08:23 -0300

    fix(routes): fix bad route import

--- app/src/main/config/routes.ts
+++ app/src/main/config/routes.ts
@@ -1,11 +1,12 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
 
   app.use('/api', router)
 
-  fg.sync('**/src/main/routes/**Routes.ts')
-    .map(async file => (await import(`../../../${file}`)).default(router))
+  readdirSync(`${__dirname}/../routes`)
+    .filter(file => !file.includes('.test.'))
+    .map(async file => (await import(`../routes/${file}`)).default(router))
 }
