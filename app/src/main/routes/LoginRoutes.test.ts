9143a9aa0b74068c6617e8ac957d708f359b9d0b
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-11 09:41:58 -0300

    test(login): ensure Login route returns 401 with invalid credentials

--- app/src/main/routes/LoginRoutes.test.ts
+++ app/src/main/routes/LoginRoutes.test.ts
@@ -55,5 +55,15 @@ describe('LoginRoutes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_mail@mail.com',
+          password: 'valid_password'
+        })
+        .expect(401)
+    })
   })
 })
