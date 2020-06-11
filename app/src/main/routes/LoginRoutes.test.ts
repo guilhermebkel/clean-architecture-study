cad718686581ee7c7a097dd58167511821392222
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-11 09:40:04 -0300

    feat(login): add login route

--- app/src/main/routes/LoginRoutes.test.ts
+++ app/src/main/routes/LoginRoutes.test.ts
@@ -1,8 +1,12 @@
 import request from 'supertest'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 import app from '../config/app'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper'
+
+let accountCollection: Collection
 
 describe('LoginRoutes', () => {
   beforeAll(async () => {
@@ -14,7 +18,7 @@ describe('LoginRoutes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
 
     await accountCollection.deleteMany({})
   })
@@ -28,6 +32,26 @@ describe('LoginRoutes', () => {
           email: 'valid_mail@mail.com',
           password: 'valid_password',
           passwordConfirmation: 'valid_password'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('valid_password', 12)
+
+      await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_mail@mail.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_mail@mail.com',
+          password: 'valid_password'
         })
         .expect(200)
     })
--- app/src/main/routes/LoginRoutes.ts
+++ app/src/main/routes/LoginRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 
 import { makeSignUpController } from '../factories/signup/SignUpFactory'
+import { makeLoginController } from '../factories/login/LoginFactory'
 import { adaptRoute } from '../adapters/ExpressRouteAdapter'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
