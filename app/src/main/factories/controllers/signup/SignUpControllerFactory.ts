64febdcd7f03d7136185b22ff3361be80a52d01e
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-15 07:31:22 -0300

    refactor(signup): move DbAddAccount creation to a factory method

--- app/src/main/factories/controllers/signup/SignUpControllerFactory.ts
+++ app/src/main/factories/controllers/signup/SignUpControllerFactory.ts
@@ -1,25 +1,16 @@
 import { SignUpController } from '../../../../presentation/controllers/signup/SignUpController'
-import { DbAddAccount } from '../../../../data/usecases/AddAccount/DbAddAccount'
-import { BcryptAdapter } from '../../../../infra/cryptography/bcryptAdapter/BcryptAdapter'
-import { AccountMongoRepository } from '../../../../infra/db/mongodb/accountRepository/AccountMongoRepository'
 import { LogMongoRepository } from '../../../../infra/db/mongodb/logRepository/LogMongoRepository'
 import { Controller } from '../../../../presentation/protocols'
 import { LogControllerDecorator } from '../../../decorators/LogControllerDecorator'
 import { makeSignUpValidation } from './SignUpValidationFactory'
 import { makeDbAuthentication } from '../../usecases/authentication/DbAuthenticationFactory'
+import { makeDbAddAccount } from '../../usecases/addAccount/DbAddAccountFactory'
 
 export const makeSignUpController = (): Controller => {
-  const SALT = 12
-
-  const bcryptAdapter = new BcryptAdapter(SALT)
-  const accountMongoRepository = new AccountMongoRepository()
   const logMongoRepository = new LogMongoRepository()
   const signUpValidation = makeSignUpValidation()
 
-  const dbAddAccount = new DbAddAccount(
-    bcryptAdapter,
-    accountMongoRepository
-  )
+  const dbAddAccount = makeDbAddAccount()
 
   const dbAuthentication = makeDbAuthentication()
 
---
+++ app/src/main/factories/usecases/addAccount/DbAddAccountFactory.ts
@@ -0,0 +1,18 @@
+import { DbAddAccount } from '../../../../data/usecases/AddAccount/DbAddAccount'
+import { BcryptAdapter } from '../../../../infra/cryptography/bcryptAdapter/BcryptAdapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/accountRepository/AccountMongoRepository'
+import { AddAccount } from '../../../../domain/usecases/AddAccount'
+
+export const makeDbAddAccount = (): AddAccount => {
+  const SALT = 12
+
+  const bcryptAdapter = new BcryptAdapter(SALT)
+  const accountMongoRepository = new AccountMongoRepository()
+
+  const dbAddAccount = new DbAddAccount(
+    bcryptAdapter,
+    accountMongoRepository
+  )
+
+  return dbAddAccount
+}
