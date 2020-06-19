54656d59801d64db1e2ad8ea4b6af7160873b898
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-19 07:31:07 -0300

    test(survey): move sut creation to a factory method

--- app/src/data/usecases/AddSurvey/DbAddSurvey.spec.ts
+++ app/src/data/usecases/AddSurvey/DbAddSurvey.spec.ts
@@ -9,19 +9,38 @@ const makeFakeSurveyData = (): AddSurveyModel => ({
   }]
 })
 
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add (data: AddSurveyModel): Promise<void> {
+      return await Promise.resolve()
+    }
+  }
+
+  const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+
+  return addSurveyRepositoryStub
+}
+
+interface SutTypes {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+
+  return {
+    sut,
+    addSurveyRepositoryStub
+  }
+}
+
 describe('DBAddSurvey Usecase', () => {
   test('Should call AddSurveyRepository with correct values', async () => {
-    class AddSurveyRepositoryStub implements AddSurveyRepository {
-      async add (data: AddSurveyModel): Promise<void> {
-        return await Promise.resolve()
-      }
-    }
-
-    const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+    const { sut, addSurveyRepositoryStub } = makeSut()
 
     const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
-
-    const sut = new DbAddSurvey(addSurveyRepositoryStub)
 
     const surveyData = makeFakeSurveyData()
 
