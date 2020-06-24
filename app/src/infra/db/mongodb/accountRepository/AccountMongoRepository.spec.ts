3e1453942b3733c6bc451911d4853da6e8ebf64a
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-24 07:56:02 -0300

    test(account): ensure AccountMongoRepository returns an account on loadByToken with role success

--- app/src/infra/db/mongodb/accountRepository/AccountMongoRepository.spec.ts
+++ app/src/infra/db/mongodb/accountRepository/AccountMongoRepository.spec.ts
@@ -113,5 +113,25 @@ describe('AccountMongoRepository', () => {
       expect(account.email).toEqual('any_mail@mail.com')
       expect(account.password).toEqual('any_password')
     })
+
+    test('Should return an account on loadByToken with role success', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+
+      const account = await sut.loadByToken('any_token', 'any_role')
+
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toEqual('any_name')
+      expect(account.email).toEqual('any_mail@mail.com')
+      expect(account.password).toEqual('any_password')
+    })
   })
 })
