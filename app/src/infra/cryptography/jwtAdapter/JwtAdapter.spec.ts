db259ae46acd95dab3b755d9b8d95a5fdff079a7
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-24 07:43:10 -0300

    test(jwt): ensure JwtAdapter throws if verify throws

--- app/src/infra/cryptography/jwtAdapter/JwtAdapter.spec.ts
+++ app/src/infra/cryptography/jwtAdapter/JwtAdapter.spec.ts
@@ -65,5 +65,17 @@ describe('JwtAdapter', () => {
 
       expect(decoded).toBe('any_value')
     })
+
+    test('Should throw if verify method throws', async () => {
+      const sut = makeSut()
+
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+      const promise = sut.decrypt('any_token')
+
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
