01bd0db8d90a0ac9e1014ee97852ec98d99ef757
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-30 07:33:49 -0300

    feat(load-surveys): ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/loadSurveys/LoadSurveysController.spec.ts
+++ src/presentation/controllers/survey/loadSurveys/LoadSurveysController.spec.ts
@@ -1,6 +1,7 @@
 import MockDate from 'mockdate'
 import { LoadSurveysController } from './LoadSurveysController'
 import { LoadSurveys, SurveyModel } from './LoadSurveysControllerProtocols'
+import { ok } from '../../../helpers/http/HttpHelper'
 
 const makeFakeSurveys = (): SurveyModel[] => ([{
   id: 'any_id',
@@ -58,5 +59,13 @@ describe('LoadSurveysController', () => {
     await sut.handle({})
 
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle({})
+
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()))
   })
 })
