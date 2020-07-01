7fb19f27bf2eb61e47f0abe841524f7248811e3e
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-07-01 07:01:57 -0300

    refactor(survey-mongo-repository): organize SurveyMongoRepository tests by describers

--- src/infra/db/mongodb/surveyRepository/SurveyMongoRepository.spec.ts
+++ src/infra/db/mongodb/surveyRepository/SurveyMongoRepository.spec.ts
@@ -25,20 +25,22 @@ describe('SurveyMongoRepository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  test('Should add a survey on success', async () => {
-    const sut = makeSut()
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const sut = makeSut()
 
-    await sut.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }],
-      date: new Date()
+      await sut.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
+
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+
+      expect(survey).toBeTruthy()
     })
-
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-
-    expect(survey).toBeTruthy()
   })
 })
