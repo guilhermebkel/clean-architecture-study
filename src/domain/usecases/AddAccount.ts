32dead5bbd64668ca7c14d4a178bbfe2060224cc
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-07-02 07:51:12 -0300

    refactor(models): use dynamic type copying instead of copying all types manually for equal types

--- src/domain/usecases/AddAccount.ts
+++ src/domain/usecases/AddAccount.ts
@@ -1,10 +1,6 @@
 import { AccountModel } from '../models/Account'
 
-export type AddAccountModel = {
-  name: string
-  email: string
-  password: string
-}
+export type AddAccountModel = Omit<AccountModel, 'id'>
 
 export interface AddAccount {
   add: (account: AddAccountModel) => Promise<AccountModel>
--- src/domain/usecases/AddSurvey.ts
+++ src/domain/usecases/AddSurvey.ts
@@ -1,10 +1,6 @@
 import { SurveyAnswerModel } from '../models/Survey'
 
-export type AddSurveyModel = {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export type AddSurveyModel = Omit<SurveyAnswerModel, 'id'>
 
 export interface AddSurvey {
   add: (data: AddSurveyModel) => Promise<void>
