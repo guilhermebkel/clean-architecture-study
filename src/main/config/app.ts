4a821f9c70b19b226d9568dd4b39d40f3f88c990
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-07-13 07:24:56 -0300

    chore(swagger): add basic docs config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 
+import setupSwagger from '@/main/config/configSwagger'
 import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
 
 const app = express()
 
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'A super clean API made with NodeJS',
+    version: '1.0.0'
+  }
+}
